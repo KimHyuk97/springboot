87eb783029b4f4ecc4af619a902f6506f10780c6
LeeJiHyeon <xnfxnf97@naver.com>
2023-02-25 00:47:35 +0900

    init

---
+++ src/main/java/com/wgilooy/spring/HelloController.java
@@ -0,0 +1,21 @@
+package com.wgilooy.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@RestController
+public class HelloController {
+
+    private final HelloService helloService;
+
+    HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return helloService.sayHello(Objects.requireNonNull(name));
+    }
+}
---
+++ src/main/java/com/wgilooy/spring/HelloService.java
@@ -0,0 +1,7 @@
+package com.wgilooy.spring;
+
+import org.springframework.stereotype.Service;
+
+public interface HelloService {
+    public String sayHello(String name);
+}
